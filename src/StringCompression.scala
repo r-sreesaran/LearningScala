82c415a5cd5245ff52eccd9d3e52ece9d793d096
r-sreesaran <r.sreesaran@gmail.com>
2014-09-13 21:28:04 +0530

    Added StringCompression

---
+++ src/StringCompression.scala
@@ -0,0 +1,13 @@
+/**
+ * Created by sree on 13/09/14.
+ */
+object StringCompression {
+  def main(args: Array[String]) {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution
+*/
+    def compression(msg:List[Char]):String = {
+
+    }
+    compression(aaabaaaaccaaaaba.toList)
+   }
+}
