031765b0999f7d55309d9e8d893580e3fbeb8a47
r-sreesaran <r.sreesaran@gmail.com>
2014-09-13 22:56:48 +0530

    Added code for StringCompresion

--- src/StringCompression.scala
+++ src/StringCompression.scala
@@ -5,9 +5,12 @@ object StringCompression {
   def main(args: Array[String]) {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution
 */
-    def compression(msg:List[Char]):String = {
+    def compression(msg:List[Char]):String  = msg match {
+      case m::msg1 => val(first,rest) = msg span (head => m == head) ; if(first.length > 1) first.head.toString + first.length + compression(rest) else first.head + compression(rest)
+      case Nil => ""
+     }
+    var list = compression(readLine().toList);
+    println(list)
 
-    }
-    compression(aaabaaaaccaaaaba.toList)
    }
 }
