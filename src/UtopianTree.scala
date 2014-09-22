807e7cdb4d2e60c966f07201e8ea5b221a220f71
r-sreesaran <r.sreesaran@gmail.com>
2014-09-22 23:31:59 +0530

    New problem

---
+++ src/UtopianTree.scala
@@ -0,0 +1,10 @@
+/**
+ * Created by sree on 22/09/14.
+ */
+object UtopianTree {
+  def main(args: Array[String]) {
+    var mapOfCycles = io.Source.stdin.getLines().take(3).map(_.toInt);
+    mapOfCycles.drop(0);
+    
+  }
+}
